d3c41aa922cec9b6880aaf5b3c78d4651cf786d4
Melony Franchini <meltravelz@gmail.com>
2023-06-07 14:18:35 -0600

    Create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_many :user_subscriptions
+  has_many :subscriptions, through: :user_subscriptions
+
+  validates_presence_of :first_name, 
+                        :last_name, 
+                        :email,
+                        :street_address,
+                        :city,
+                        :state,
+                        :zip_code
+  validates_uniqueness_of :email 
+
 end
