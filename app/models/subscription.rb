54cd71bf01840dcc23475c436db07a7657f96d41
Melony Franchini <meltravelz@gmail.com>
2023-06-07 14:20:05 -0600

    Create subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,10 @@
 class Subscription < ApplicationRecord
+  has_many :user_subscriptions
+  has_many :users, through: :user_subscriptions
+
+  validates :title, presence: true, uniqueness: true
+  validates :price_usd, presence: true, numericality: true
+  # do not need validation for frequency since it has a default value
+
+  enum frequency: ["monthly", "bi-monthly", "quarterly"]
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should have_many :user_subscriptions }
+    it { should have_many(:users).through(:user_subscriptions) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_uniqueness_of :title }
+    
+    it { should validate_presence_of :price_usd }
+    it { should validate_numericality_of :price_usd }
+  end
+
+  describe "enums" do
+    it { should define_enum_for(:frequency).with_values(["monthly", "bi-monthly", "quarterly"]) }
+  end
 end
