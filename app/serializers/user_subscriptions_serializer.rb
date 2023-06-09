bf26a101dfe31a3199b4e2a3da4413c420c86c90
Melony Franchini <meltravelz@gmail.com>
2023-06-09 17:08:43 -0600

    Comment out second serializer

--- app/serializers/user_subscriptions_serializer.rb
+++ app/serializers/user_subscriptions_serializer.rb
@@ -1,4 +1,6 @@
-class UserSubscriptionsSerializer
-  include JSONAPI::Serializer
-  attributes :all_sub_info
-end
+# Phase 1 Usering Presenter:
+
+# class UserSubscriptionsSerializer
+#   include JSONAPI::Serializer
+#   # attributes :all_sub_info
+# end
