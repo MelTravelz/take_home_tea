f497c5302944b6a71e1cdccf588b6849fccab43a
Melony Franchini <meltravelz@gmail.com>
2023-06-09 17:50:29 -0600

    Refactor tests & code using delegation

--- app/serializers/user_subscription_serializer.rb
+++ app/serializers/user_subscription_serializer.rb
@@ -1,5 +1,5 @@
 class UserSubscriptionSerializer
   include JSONAPI::Serializer
   # when using delegation, only one serializer is needed and all attributes listed here:
-  # attributes :title, :price, :status, :frequency
+  attributes :title, :price_usd, :status, :frequency
 end
